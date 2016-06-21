1e4686b5b4ba00a4ef5cfec7d0cfb81a6ee116f9
Suo Qin <kpsesuo@gmail.com>
2016-06-21 20:22:15 +1000

    read environment variables

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-var jsonfile = require('jsonfile')
+var jsonfile = require('jsonfile');
+
+var http = require('http');
+
+console.log(process.env.username);
+console.log(process.env.password);
 
 var file = './out/data.json'
-var obj = {name: 'JP'}
+var obj = {
+  username: process.env.username,
+  password: process.env.password
+}
 
 jsonfile.writeFile(file, obj, function (err) {
   console.error(err)
