2f5854360b56b4df6c29c53ebd5ce1efcb413efd
Suo Qin <kpsesuo@gmail.com>
2016-06-21 20:45:56 +1000

    retrieve all school information

--- main.js
+++ main.js
@@ -1,16 +1,51 @@
 var jsonfile = require('jsonfile');
+var _ = require('lodash');
 
-var http = require('http');
 
 console.log(process.env.username);
 console.log(process.env.password);
 
-var file = './out/data.json'
-var obj = {
-  username: process.env.username,
-  password: process.env.password
-}
 
-jsonfile.writeFile(file, obj, function (err) {
-  console.error(err)
-})
+var credential = {
+  account_name: process.env.username,
+  password: process.env.password
+};
+
+var Client = require('node-rest-client').Client;
+
+var client = new Client();
+
+var args = {
+  data: credential,
+  headers: { "Content-Type": "application/json" }
+};
+
+var host = "https://stage2.cocobabys.com";
+var loginUrl = host + "/employee_login.do";
+var allSchools = host + "/kindergarten";
+
+var schoolUrl = function(id) {
+  return host + "/kindergarten/" + id;
+};
+
+client.post(loginUrl, args, function (data, response) {
+  // parsed response body as js object
+  console.log(data);
+  // raw response
+  console.log(response);
+  writeToFile('school', data);
+  client.get(allSchools, {}, function (data) {
+    _.each(data, function (s) {
+      console.log(s.schoolId);
+      writeToFile(s.schoolId, s);
+    })
+  })
+});
+
+
+function writeToFile(filename, obj) {
+  var file = './out/' + filename + '.json';
+  jsonfile.writeFile(file, obj, function (err) {
+    console.error('err', err);
+  });
+}
