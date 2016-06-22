408ac2672a3fed5a778a7740280573abf984e81e
Suo Qin <kpsesuo@gmail.com>
2016-06-22 22:13:19 +1000

    transform dist json, ignore ref folder

--- main.js
+++ main.js
@@ -42,9 +42,8 @@ client.post(loginUrl, args, function (data, response) {
   var cookies = transferCookie(response);
   client.get(allSchools, cookies, function (all) {
     var schools = all;
-    console.log(schools[0]);
     _.each([schools[1]], function (s) {
-      console.log('s.school_id = ', s.school_id);
+      console.log('s.school_id =', s.school_id);
       outputSchool(s, cookies)
     })
   })
@@ -76,7 +75,6 @@ function outputSchool(school, cookie) {
       "school_linkphone": s.phone
     }
   };
-  console.log(content);
 
   var employeesDefer = Q.defer();
   client.get(employeeUrl(school), cookie, function (all) {
@@ -96,17 +94,18 @@ function outputSchool(school, cookie) {
   });
   var promiseOfClasses = classDefer.promise;
 
-  Q.all([promiseOfEmployees, promiseOfRelationships, promiseOfClasses]).then(function(arr) {
+  Q.all([promiseOfEmployees, promiseOfRelationships, promiseOfClasses]).then(function (arr) {
     var employees = arr[0];
     var relationships = arr[1];
     var classes = arr[2];
-    console.log("employees: ", employees);
-    console.log("relationships: ", relationships);
-    content['master_info'] = employees[0];
-    content['teacher_list'] = employees;
-    content['class_list'] = classes;
-    content['parent_list'] = relationships;
-    content['child_list'] = relationships;
+
+    content['master_info'] = pickUpPrincipal(employees);
+    content['teacher_list'] = transformEmployees(employees);
+    content['class_list'] = transformClass(classes);
+    content['parent_list'] = transformParents(relationships);
+    content['child_list'] = transformChildren(relationships);
+    // console.log('content', content);
+
     writeToFile(school.full_name, content);
     console.log('school done: ' + school.school_id);
   });
@@ -120,4 +119,83 @@ function cityOf(address) {
 }
 function areaOf(address) {
   return address;
+}
+
+function transformClass(classes) {
+  return _.map(classes, function (c) {
+    return {
+      "source_class_id": c.class_id.toString(),
+      "class_name": c.name,
+      "is_graduation": "0"
+    };
+  });
+}
+
+function transformParents(relationships) {
+  // console.log('transformParents', relationships);
+  return _.map(relationships, function (r) {
+    return {
+      "source_parent_id": r.parent.parent_id,
+      "mobile": r.parent.phone,
+      "name": r.parent.name,
+      "password": pickUpParentPassword(r.parent.phone),
+      "source_child_id": r.child.child_id,
+      "relation_type": ""//与孩子的关系，4-爸爸,5-妈妈,6-爷爷,7-姥姥,8-亲属,10-姥爷,11-奶奶
+    };
+  })
+}
+function transformChildren(relationships) {
+  // console.log('transformChildren');
+  return _.map(relationships, function (r) {
+    return {
+      "source_child_id": r.child.child_id,
+      "name": r.child.name,
+      "source_class_id": r.child.class_id.toString(),
+      "birthday": r.child.birthday + " 00:00:00",
+      "sex": genderDisplay(r.child.gender), //0-未知，1-男，2-女
+      "is_graduation": "0", //0-否，1-是
+      "is_in_school": "1" //0-否，1-是
+    };
+  })
+}
+
+function transformEmployees(employees) {
+  // console.log('transformEmployees', employees);
+  return _.map(employees, function (e) {
+    return {
+      "source_teacher_id": e.id,
+      "mobile": e.phone,
+      "name": e.name,
+      "password": pickUpEmployeePassword(e.phone),
+      "birthday": e.birthday + " 00:00:00",
+      "sex": genderDisplay(e.gender), //0-未知，1-男，2-女
+      "source_class_id": "??"
+    };
+  });
+}
+
+function pickUpPrincipal(employees) {
+  // console.log('pickUpPrincipal', employees);
+  var principal = _.find(employees, function (e) {
+    return e.privilege_group == 'principal';
+  });
+  return principal == undefined ? {} : {
+    "source_master_id": principal.id,
+    "mobile": principal.phone,
+    "name": principal.name,
+    "password": pickUpEmployeePassword(principal.phone),
+    "introduction": ""
+  };
+}
+
+function genderDisplay(gender) {
+  return gender == 0 ? '1' : '2';
+}
+
+function pickUpParentPassword(phone) {
+  return phone;
+}
+
+function pickUpEmployeePassword(phone) {
+  return phone;
 }
