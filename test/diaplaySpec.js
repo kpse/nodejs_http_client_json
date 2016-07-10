f639337597686cd7ecb68df0b4aedbba8dd3c6ff
Suo Qin <kpsesuo@gmail.com>
2016-07-10 19:32:55 +1000

    add gender converting test

---
+++ test/diaplaySpec.js
@@ -0,0 +1,15 @@
+var display = require('../src/display');
+var assert = require('chai').assert;
+
+describe('Display', function () {
+  describe('gender()', function () {
+    it('should return 1 for 0', function () {
+      assert.equal('1', display.gender('0'));
+    });
+    it('should return 2 for other', function () {
+      assert.equal('2', display.gender('1'));
+      assert.equal('2', display.gender('3'));
+      assert.equal('2', display.gender('-1'));
+    });
+  });
+});
