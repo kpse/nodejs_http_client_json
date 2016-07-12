3479df6845a91e1d6d3eeed3cd8810f6c56e238e
Suo Qin <kpsesuo@gmail.com>
2016-07-12 21:57:48 +1000

    transform children and employee cards

--- cards.js
+++ cards.js
@@ -31,8 +31,16 @@ Q.all([promiseOfChildrenCard, promiseOfEmployeeCard, promiseOfEmptyCard, promise
   console.log(unused[0]);
   console.log(schoolInfo);
 
+  var employeesDic = _.groupBy(employees, SCHOOL_FIELD);
+  var childrenDic = _.groupBy(children, SCHOOL_FIELD);
 
-  file.write('空白卡', unusedTransform(unused));
+  _.each(_.keys(schoolInfo), function (school) {
+    outputCards(school, schoolInfo[school.toString()].full_name, employeesDic[school.toString()] || [],
+      childrenDic[school.toString()] || []);
+  });
+
+
+  // file.write('空白卡', unusedTransform(unused));
   // console.log('school done: ' + school.school_id);
 
 });
@@ -50,4 +58,51 @@ function unusedTransform(info) {
       })
     }
   };
+}
+
+var outputCards = function (school, name, employees, children) {
+
+  console.log('school cards starting: ' + school);
+  var content = {
+    "card_info": {
+      "school_id": school.toString(),
+      "school_name": name,
+      "card_list": _.flatten(employeeCards(employees), childrenCards(children))
+    }
+  };
+
+  file.write(name, content);
+  console.log('school cards done: ' + school);
+};
+
+function employeeCards(employees) {
+  return _.map(employees, function (e) {
+    return {
+      "card_no": e.card,
+      "mac_id": e.mac,
+      "status": "0",
+      "type": "0",
+      "person_id": e.employee_id,
+      "person_name": e.name,
+      "create_time": "2016-06-12 18:18:18",
+      "origin": "youlebao"
+    };
+  })
+}
+
+function childrenCards(children) {
+  return _.map(children, function (c) {
+    return {
+      "card_no": c.card,
+      "mac_id": c.mac,
+      "status": "0",
+      "type": "1",
+      "person_id": c.child_id,
+      "person_name": c.name,
+      "class_id": c.class_id,
+      "class_name": c.class_name,
+      "create_time": "2016-06-12 18:18:18",
+      "origin": "youlebao"
+    };
+  });
 }
