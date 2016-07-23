9fc325d64f2eb78bc41710995b866670b36dfb3d
Suo Qin <kpsesuo@gmail.com>
2016-07-23 12:18:44 +1000

    add test for grandfather translating

--- test/relationshipSpec.js
+++ test/relationshipSpec.js
@@ -6,8 +6,11 @@ describe('Relationship', function () {
     it('should return 4 for father', function () {
       assert.equal('4', relationship('爸爸'));
     });
-    it('should return 4 for mother', function () {
+    it('should return 5 for mother', function () {
       assert.equal('5', relationship('妈妈'));
+    });
+    it('should return 6 for grandfather', function () {
+      assert.equal('6', relationship('爷爷'));
     });
   });
 });
