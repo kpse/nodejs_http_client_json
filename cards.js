af87576041ba1ef8857cdab2edbe836a95034e97
Suo Qin <kpsesuo@gmail.com>
2016-08-04 21:12:33 +1000

    revise card information

--- cards.js
+++ cards.js
@@ -3,7 +3,8 @@ var Q = require('q');
 var csv = require('./src/parseCSV');
 var file = require('./src/file');
 
-var env = '';
+// var env = '';
+var env = 'stage2.';
 
 var mapCSV = csv.mapCSV;
 var parseCSV = csv.parseCSV;
@@ -64,14 +65,12 @@ var outputCards = function (school, name, employees, children) {
 
   console.log('school cards starting: ' + school);
   var content = {
-    "card_info": {
-      "school_id": school.toString(),
+      "source_school_id": school.toString(),
       "school_name": name,
       "card_list": _.flatten(employeeCards(employees), childrenCards(children))
-    }
   };
 
-  file.write(name, content);
+  file.write('device_' + school + '_' + name, content);
   console.log('school cards done: ' + school);
 };
 
@@ -79,13 +78,11 @@ function employeeCards(employees) {
   return _.map(employees, function (e) {
     return {
       "card_no": e.card,
-      "mac_id": e.card,
       "status": "0",
       "type": "0",
-      "person_id": e.employee_id,
+      "source_person_id": e.employee_id,
       "person_name": e.name,
-      "create_time": "2016-06-12 18:18:18",
-      "origin": "youlebao"
+      "create_time": "2016-06-12 18:18:18"
     };
   })
 }
@@ -94,15 +91,13 @@ function childrenCards(children) {
   return _.map(children, function (c) {
     return {
       "card_no": c.card,
-      "mac_id": c.card,
       "status": "0",
       "type": "1",
-      "person_id": c.child_id,
+      "source_person_id": c.child_id,
       "person_name": c.name,
-      "class_id": c.class_id,
+      "source_class_id": c.class_id,
       "class_name": c.class_name,
-      "create_time": "2016-06-12 18:18:18",
-      "origin": "youlebao"
+      "create_time": "2016-06-12 18:18:18"
     };
   });
 }
