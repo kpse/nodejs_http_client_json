74b3d88e3e6bac305f5b17916b6d1572bbc6a14c
Suo Qin <kpsesuo@gmail.com>
2016-08-21 18:25:08 +1000

    add a city test

--- test/addressSpec.js
+++ test/addressSpec.js
@@ -11,6 +11,7 @@ describe('Address', function () {
       it('should return city of given address string', function () {
         assert.equal('成都市', address.cityOf('四川省成都市'));
         assert.equal('深圳市', address.cityOf('广东省深圳市'));
+        assert.equal('', address.cityOf('四川省双流县'));
       });
     });
     describe('areaOf()', function () {
