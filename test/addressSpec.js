a90affa9d27de10b991f4223fa26d181f02c30f8
Suo Qin <kpsesuo@gmail.com>
2016-09-02 22:56:41 +1000

    extract Hong Kong address

--- test/addressSpec.js
+++ test/addressSpec.js
@@ -6,6 +6,7 @@ describe('Address', function () {
     it('should return province of given address string', function () {
       assert.equal('四川省', address.provinceOf('四川省成都市'));
       assert.equal('广东省', address.provinceOf('广东省深圳市'));
+      assert.equal('香港特别行政区', address.provinceOf('香港特别行政区'));
     });
     describe('cityOf()', function () {
       it('should return city of given address string', function () {
