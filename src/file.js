962ec3d58a252323d2698c7562a61f9afb35c0cd
Suo Qin <kpsesuo@gmail.com>
2017-04-14 19:33:15 +1000

    use ES6 syntax

--- src/file.js
+++ src/file.js
@@ -1,28 +1,31 @@
-var jsonfile = require('jsonfile');
-var fs = require('fs');
+'use strict';
+
+const jsonfile = require('jsonfile');
+const fs = require('fs');
 
 
 function writeToFile(filename, obj) {
-  var file = './out/' + filename + '.json';
+  const file = './out/' + filename + '.json';
   jsonfile.writeFile(file, obj, function (err) {
     if (err) console.error('err', err);
   });
 }
 
 function dynamicInfoOutput(filename, obj) {
-  var file = './out-dynamic/dynamic-' + filename + '.json';
+  const file = './out-dynamic/dynamic-' + filename + '.json';
   jsonfile.writeFile(file, obj, function (err) {
     if (err) console.error('err', err);
   });
 }
 
-var isFileExisting = function (name) {
+const isFileExisting = function (name) {
   try {
     fs.accessSync('./out/' + name + '.json', fs.F_OK);
     return true;
   } catch (e) {
+    console.log(e);
+    return false;
   }
-  return false;
 };
 
 
