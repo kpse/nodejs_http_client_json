20b9d3c624c873371cbd295a10d4cb531cd732cf
Suo Qin <kpsesuo@gmail.com>
2017-04-22 10:58:08 +1000

    output to different folder in apis

--- src/file.js
+++ src/file.js
@@ -4,33 +4,53 @@ const jsonfile = require('jsonfile');
 const fs = require('fs');
 
 
+const folderCheck = dir => {
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
+}
 function write(filename, obj) {
-  const file = './out/' + filename + '.json';
-  jsonfile.writeFile(file, obj, function (err) {
+  folderCheck('./out/dataSync')
+  const file = './out/dataSync/' + filename + '.json';
+  jsonfile.writeFile(file, obj, (err) => {
+    if (err) console.error('err', err);
+  });
+}
+
+function writeCard(filename, obj) {
+  folderCheck('./out/cardSync')
+  const file = './out/cardSync/' + filename + '.json';
+  jsonfile.writeFile(file, obj, (err) => {
     if (err) console.error('err', err);
   });
 }
 
 function dynamicOutput(filename, obj) {
-  const file = './out-dynamic/dynamic-' + filename + '.json';
+  folderCheck('./out/dynamicSync')
+  const file = './out/dynamicSync/dynamic-' + filename + '.json';
   jsonfile.writeFile(file, obj, function (err) {
     if (err) console.error('err', err);
   });
 }
 
-const isExisting = function (name) {
+const fileCheck = fileName => {
   try {
-    fs.accessSync('./out/' + name + '.json', fs.F_OK);
+    fs.accessSync(fileName, fs.F_OK);
     return true;
   } catch (e) {
     console.log(e);
     return false;
   }
-};
+}
 
+const isExisting = name => fileCheck('./out/' + name + '.json')
+
+const isDynamicExisting = name => fileCheck('./out/dynamicSync/dynamic-' + name + '.json')
 
 module.exports = {
   write,
+  writeCard,
   isExisting,
+  isDynamicExisting,
   dynamicOutput
 };
