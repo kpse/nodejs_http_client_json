1ccfb622494a288d4df6e10596d59497c94e29a1
Suo Qin <kpsesuo@gmail.com>
2017-05-06 10:12:16 +1000

    curry the lambda

--- src/classesFunctions.js
+++ src/classesFunctions.js
@@ -5,9 +5,9 @@ const _ = require('lodash');
 module.exports = (allClasses, subordinate, employee) => {
   // console.log('allClasses', allClasses);
   // console.log('subordinate', subordinate);
-  const ret = _.filter(subordinate, c => _.some(allClasses, target => c.toString() == target));
+  const ret = _.filter(subordinate, c => _.some(allClasses, _.curry(_.eq)(c)));
 
-  if(ret.length !== subordinate.length) {
+  if(!_.eq(ret.length, subordinate.length)) {
     console.log('employees subordinates filtered? ', allClasses, _.compact(subordinate), ret, employee);
   }
 
