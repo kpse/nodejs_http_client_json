b7ceb19c42f338c1e892fa3d5b313c104aa91ca5
Suo Qin <kpsesuo@gmail.com>
2017-07-02 13:57:08 +1000

    split families into schools

--- test/newExportSpec.js
+++ test/newExportSpec.js
@@ -19,6 +19,24 @@ describe('New export', () => {
       expect(structure.classes[0].parents).to.deep.equal([{name: "parent1", phone: '789', relationship: 'father'}]);
     });
 
+    it('should split families into schools', () => {
+      const families = {
+        '123': [{name: 'child1', parent: 'parent1', phone: '789', class_id: 456, school_id: 123, relationship: 'father'}],
+        '199': [{name: 'child99', parent: 'parent99', phone: '1789', class_id: 999, school_id: 199, relationship: 'father'}]
+      };
+      const fillIn = exportFunctions.fillIn;
+      let structure = fillIn(families)([{school_id: 123, class_id: 456}]);
+
+      expect(structure).to.have.property('id', 123);
+
+      let structure2 = fillIn(families)([{school_id: 199, class_id: 999}, {school_id: 199, class_id: 998}]);
+
+      expect(structure2).to.have.property('id', 199);
+      expect(structure2.classes).to.have.lengthOf(1);
+      expect(structure2.classes[0].children).to.deep.equal([{name: "child99", phone: '1789'}]);
+      expect(structure2.classes[0].parents).to.deep.equal([{name: "parent99", phone: '1789', relationship: 'father'}]);
+    });
+
     it('should split family into classes', () => {
       const families = {
         '123': [
